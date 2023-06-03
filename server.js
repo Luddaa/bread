4e2a7277b475572e8c88127c0a48d591bf78f3e7
Luddaa <dangeloeugene33@gmail.com>
2023-06-03 11:29:24 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -12,14 +12,21 @@ app.use('/breads',breadsController)
 app.set('views',__dirname + '/views')
 app.set('view engine','jsx')
 app.engine('jsx',require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 //Routes
 app.get('/',(req,res)=>{
 res.send("Welcome  to an Awesome App about Bread!")
 })
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 //Listen
 app.listen(PORT,() => {
     console.log('listening on port', PORT);
 })
+
